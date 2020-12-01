7560e9eed4eac5eb26050f5338bf868a344c84de
root <root@SREYA-PC.localdomain>
2020-12-01 11:43:12 -0800

    updates regex

--- numbers.sh
+++ numbers.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 # numbers.sh
-
+# Sreya Vadlamudi
+echo -n "Enter a positive integer: "
+read n
+N=1
+while [ "$N" -le "$n" ]
+do
+	echo -n $N
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo " Even "
+	else
+		echo " Odd "
+	fi
+	N=$(($N+1))
+done
